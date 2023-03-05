a1ea79f9da0355d2fc4c9c28452acf02a231e61d
Noah Bertholon <noahbertholon@gmail.com>
2023-03-05 23:40:19 +0100

    début classe plateau
    Début de l'implémentation de la classe plateau.
    Cette classe doit être aussi légère que possible, car elle sera présente
    dans chaque noeud du graph.
    D'où le choix de ne garder qu'un tableau de bloc (qui sont déjà optimisé
    en mémoire) et quelques informations supplémentaires.
    Mais pas de représentation du plateau sous forme de tableau (ce qui
    optimiserais le temps d'exécution mais grossirait grandement la taille
    en mémoire). Puisqu'il n'y aura jamais plus de 16 blocs dans un plateau,
    même s'il l'ont doit parcourir tous ces blocs, cela ne représentera pas
    une charge trop importante.
    On pourra étudier la nécessité d'une bitmap pour représenter le plateau
    de jeu, ce qui sera plus optimisé en mémoire (qu'un tableau d'octet), et plus optimisé en
    exécution. Mais étant donné qu'il y a 36 cases, il faudra à minima
    contenir 64 bits, donc beaucoup d'espace perdu.
    
    La classe plateau contient une méthode move_block qui déplacera un bloc,
    instanciera un nouveau plateau, et renverra le retournera.
    Cette méthode sera utile dans le parcours de graph pour visiter des
    nouvelles configurations.
    La gestion de la durée de vie de ces pointeurs sur plateau sera laissée
    au graph. Les plateaux ne s'occupent ni de leur enfant (ce qui couterait
    en espace mémoire), ni d'eux même (puisque le graph aura besoin de leur
    instance jusqu'à ce qu'il soit détruit).
    
    Enfin, des méthodes pour charger un plateau depuis un fichier ont été
    prévu. Peut être que celles-ci seront déplacées dans la class graph par
    la suite.

--- src/plateau.cpp
+++ src/plateau.cpp
@@ -1,2 +1,76 @@
 #include "plateau.hpp"
 
+Plateau::Plateau(std::size_t nb_blocks) :
+    m_blocks_array(new Bloc[nb_blocks]), m_blocks_count(nb_blocks)
+{}
+
+Plateau::Plateau(std::string file_path) :
+    m_blocks_array(load_file(file_path)), 
+    m_blocks_count(get_block_count_from_file(file_path))
+{
+}
+
+Plateau::~Plateau() {
+    delete [] m_blocks_array;
+}
+
+Bloc* Plateau::load_file(std::string file_path) {
+    // TODO: implémenter lecture de fichier
+    return nullptr;
+}
+
+std::size_t Plateau::get_block_count_from_file(std::string file_path) {
+    // TODO: implémenter lecture nombre de bloc
+    return 0;
+}
+
+bool Plateau::can_block_move(const Bloc& block, int displacement) const {
+    int2 coords = block.get_coord();
+    Orientation o = block.get_orientation();
+    uint8_t size = block.get_size();
+
+    switch(o) {
+    case Orientation::horizontal:
+        coords.x += displacement + (displacement > 0 ? size - 1 : 0);
+        break;
+    case Orientation::vertical:
+        coords.y += displacement + (displacement > 0 ? size - 1 : 0);
+        break;
+    }
+
+    return is_cell_empty(coords);
+}
+
+bool Plateau::is_cell_empty(int2 coords) const {
+    int2 block_coords;
+    uint8_t block_size;
+    for(int i = 0; i < m_blocks_count; i++) {
+        block_coords = m_blocks_array[i].get_coord();
+        block_size = m_blocks_array[i].get_size();
+
+        switch(m_blocks_array[i].get_orientation()) {
+        case Orientation::horizontal:
+            if(block_coords.y == coords.y && block_coords.x <= coords.x && block_coords.x + block_size >= coords.x)
+                return false;
+            break;
+        case Orientation::vertical:
+            if(block_coords.x == coords.x && block_coords.y <= coords.y && block_coords.y + block_size >= coords.y)
+                return false;
+            break;
+        }
+    }
+    return true;
+}
+
+std::unique_ptr<Plateau> Plateau::move_block(const Bloc& block, int displacement) const {
+    std::unique_ptr<Plateau> new_board;
+
+    // TODO: implémenter la génération d'un nouveau plateau
+
+    return new_board;
+}
+
+bool Plateau::test() {
+    // TODO: implémenter la fonction de test
+    return true;
+}
--- src/plateau.hpp
+++ src/plateau.hpp
@@ -1,18 +1,84 @@
 #ifndef __PLATEAU__
 #define __PLATEAU__
 
-/*
-Plateau de 6x6
-Jamais plus de 16 voitures
+#include "bloc.hpp"
+#include <memory>
+#include <string>
 
-
-*/
 
 class Plateau {
 public:
+    /**
+     * @brief Constructeur pour les tests, ne sera pas utilisé dans la version finale
+     * @param nb_blocks Nombre de blocs à créer
+     */
+    Plateau(std::size_t nb_blocks);
+
+    /**
+     * @brief Constructeur du plateau. Lit un fichier et instancie le bon nombre de blocs
+     * et les variables membres.
+     * 
+     * @param file_path Chemin vers le fichier de jeu
+     */
+    Plateau(std::string file_path);
+    ~Plateau();
+
+    /**
+     * @brief Vérifie si un bloc peut se déplacer d'une certaine distance
+     * Suppose que les déplacements antérieurs sont possible.
+     *   Ne vérifie que si l'extrémité du bloc arrive sur une case vide.
+     * 
+     * @param block Une référence sur un bloc
+     * @param displacement La distance à laquelle se déplacer
+     *      <0 : Coin supérieur gauche
+     *      >0 : Coin inférieur droit
+     * @return true Le bloc peut se déplacer
+     * @return false Le bloc ne peut pas se déplacer
+     */
+    bool can_block_move(const Bloc& block, int displacement) const;
+
+    /**
+     * @brief Renvoie un pointeur sur plateau dans la nouvelle configuration (après déplacement)
+     * La durée de vie du pointeur doit être gérer par le graph
+     * 
+     * @param block_index Une référence sur un bloc
+     * @param displacement La distance à laquelle se déplacer
+     *      <0 : Coin supérieur gauche
+     *      >0 : Coin inférieur droit
+     * @return std::weak_ptr<Plateau> 
+     */
+    std::unique_ptr<Plateau> move_block(const Bloc& block, int displacement) const;
+
+    static bool test();
 
 private:
-    
+    /** @brief Tableau de bloc, à tenir à jour après chaque màj du plateau de jeu
+     *   On l'utilisera pour récupérer directement la version condensée du plateau
+     *   de jeu, pour réduire le temps d'exécution lors de la génération des noeuds
+     * du graph
+     */
+    Bloc* m_blocks_array;
+
+    /**
+     * @brief Stock le nombre de blocs contenu dans le plateau de jeu
+     */
+    std::size_t m_blocks_count;
+
+    /**
+     * @brief Vérifie si la cellule est occupé par un bloc
+     * 
+     * @param coords Coordonnée testée
+     * @return true La cellule contient un bloc
+     * @return false La cellule ne contient pas de bloc
+     */
+    bool is_cell_empty(int2 coords) const;
+
+    /// Charge un fichier en mémoire. Renvoie un tableau de blocs
+    static Bloc* load_file(std::string file_path);
+
+    /// Renvoie le nombre de blocs du plateau en lisant depuis un fichier
+    /// nécessaire pour instancier m_blocs_count
+    static std::size_t get_block_count_from_file(std::string file_path);
 };
 
 #endif
