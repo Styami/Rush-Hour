319a3b7224cbcc5cad53f9e68eb2e097022362a0
Stéphane <stephane.bomard@etu.univ-lyon.fr>
2023-03-25 13:52:51 +0100

    modification et réorganisation du main.

--- src/main.cpp
+++ src/main.cpp
@@ -5,29 +5,12 @@
 int main(){
     std::srand(std::time(nullptr));
     int r=rand()%10;
-    std::vector<Sommets<int>*> lil_graph;
-    Sommets<int>* init_noeud= new Sommets<int>(r);
-    std::unordered_map<int,Sommets<int>*> lil_graph_map;
-    //(lil_graph.begin(),lil_graph.end());
-    lil_graph_map[r]= init_noeud;
-    Graph<int> graph=Graph(lil_graph_map);
 
-    /*for (int i=0; i<15; i++) {
-        lil_graph.push_back(new Sommets<int>(i));
-    }
-    lil_graph[0]->link(lil_graph[2]);
-    lil_graph[2]->link(lil_graph[1]);
-    lil_graph[2]->link(lil_graph[7]);
-    lil_graph[1]->link(lil_graph[3]);
-    lil_graph[7]->link(lil_graph[8]);
-    lil_graph[7]->link(lil_graph[10]);
-    lil_graph[8]->link(lil_graph[9]);
-    lil_graph[10]->link(lil_graph[11]);
-    lil_graph[11]->link(lil_graph[12]);
-    lil_graph[13]->link(lil_graph[13]);
-    lil_graph[13]->link(lil_graph[14]);*/
-    //Graph<int> graph= Graph(lil_graph, lil_graph_map);
-    graph.parcours(5);
+    Sommets<int>* init_noeud= new Sommets<int>(r); //création du noeud initial
+    Graph<int> graph=Graph(init_noeud);
+
+    int nb_voisins_node=3;
+    graph.parcours(5,nb_voisins_node);
     
     return 0;
 }
